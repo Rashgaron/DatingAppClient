073c42bd4e6872cdac9a09d6630109479cfc9b54
Rashgaron <45242200+Rashgaron@users.noreply.github.com>
2021-11-27 21:40:24 +0100

    Request users from API Show users onto UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Client';
+  users: User[]; 
+
+  constructor( private http: HttpClient ){}
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  private getUsers() {
+    this.http.get<User[]>("https://localhost:5001/api/users").subscribe(users => {
+      this.users = users;
+    },
+      error => { console.log(error); });
+  }
 }
